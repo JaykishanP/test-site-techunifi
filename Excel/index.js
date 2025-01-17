83b0633b4062e21513867f5654707e923d87471b
SahanaLRGowda <160768472+SahanaLRGowda@users.noreply.github.com>
2025-01-17 13:40:36 +0530

    Add files via upload

--- Excel/index.js
+++ Excel/index.js
@@ -23,7 +23,10 @@ app.use(
   })
 );
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // Adjust size as needed
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 
 // Google Sheets API setup
 const auth = new google.auth.GoogleAuth({
