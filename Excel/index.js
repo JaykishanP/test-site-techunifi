9bc302c1df18bbb5f7c6e76cb59fe470a0e1fc4f
SahanaLRGowda <160768472+SahanaLRGowda@users.noreply.github.com>
2025-01-20 18:53:42 +0530

    Add files via upload

--- Excel/index.js
+++ Excel/index.js
@@ -45,7 +45,7 @@ const appendToSheet = async (spreadsheetId, data) => {
     console.log('Data to append:', data); // Log the incoming data
     const response = await sheets.spreadsheets.values.append({
       spreadsheetId,
-      range: `${SHEET_NAME}!A:J`, // Adjusted range
+      range: `${SHEET_NAME}!A28:J`, // Adjusted range
       valueInputOption: 'USER_ENTERED',
       resource: {
         values: [ // Wrap the data array inside another array (which it looks like you're already doing)
