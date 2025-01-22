66ee22e9b160d712c819a6e8eb172f20dcde925f
SahanaLRGowda <160768472+SahanaLRGowda@users.noreply.github.com>
2025-01-22 19:03:49 +0530

    Add files via upload

--- Excel/index.js
+++ Excel/index.js
@@ -24,8 +24,9 @@ app.use(
 );
 
 // app.use(express.json());
-app.use(express.json({ limit: '20mb' })); // Adjust the limit as per your needs
-app.use(express.urlencoded({ limit: '20mb', extended: true })); // For URL-encoded bodies
+app.use(express.json({ limit: '20mb' })); // For JSON bodies
+app.use(express.urlencoded({ limit: '20mb', extended: true })); // For form data
+
 
 
 // Google Sheets API setup
@@ -83,6 +84,7 @@ const uploadFileToDrive = async (fileName, base64File, mimeType) => {
 
 // API Endpoint to handle form submission
 app.post('/submit-timesheet', async (req, res) => {
+  console.log(req.body); // Debug incoming request body
   try {
     const {
       userName,
