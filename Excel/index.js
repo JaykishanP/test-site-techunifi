e5643b35805928459f617f9c7bdb91a8a249801f
SahanaLRGowda <160768472+SahanaLRGowda@users.noreply.github.com>
2025-03-24 20:50:04 +0530

    Add files via upload

--- Excel/index.js
+++ Excel/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
+// Middleware
 const allowedOrigins = ['https://www.techunifi.com']; // Add allowed frontend origins here
 
 app.use(
@@ -36,7 +36,7 @@ const auth = new google.auth.GoogleAuth({
 const sheets = google.sheets({ version: 'v4', auth });
 
 // Constants
-const SPREADSHEET_ID = '17xp08DNwal5DTc2pd8I8x-a1TQCQZZr_Oy882MIu_TU'; // Replace with your spreadsheet ID
+const SPREADSHEET_ID = '1Zm7ELsVKDi2w4TlZbs0b-kSIOjiPdWr9oWqmV8-Va0I'; // Replace with your spreadsheet ID
 const SHEET_NAME = 'Timesheet';
 
 // Function to append data to Google Sheet
